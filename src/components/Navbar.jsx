384e1cc230710f8b22a742f9cfde0f8f7d708eee
navitat <anlon636@gmail.com>
2025-08-04 17:00:50 +0200

    feat: add conditional login and signup buttons on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,34 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 
 function Navbar() {
+  const { isLoggedIn, user } = useContext(AuthContext);
   return (
     <nav>
       <Link to="/">
         <button>Home</button>
       </Link>
 
-      <Link to="/projects">
-        <button>Projects</button>
-      </Link>
+      {isLoggedIn && (
+        <>
+          <Link to="/projects">
+            <button>Projects</button>
+          </Link>
+          <button>Logout</button>
+        </>
+      )}
+
+      {!isLoggedIn && (
+        <>
+          <Link to="/signup">
+            <button>Sign Up</button>
+          </Link>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </>
+      )}
     </nav>
   );
 }
