a97de1ad2873b2892f742f44ddcb9fa7237adac7
navitat <anlon636@gmail.com>
2025-08-04 16:54:28 +0200

    feat (auth): implement auth context component

---
+++ src/context/auth.context.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const API_URL = "http://localhost:5005";
+
+const AuthContext = React.createContext();
+
+function AuthProviderContext(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  // auth status handler functions
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, isLoading, user }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthProviderContext, AuthContext };
