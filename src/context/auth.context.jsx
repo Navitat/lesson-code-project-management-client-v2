047404ae957617d242980a5fe6bbfa3cd0abf415
navitat <anlon636@gmail.com>
2025-08-04 16:56:29 +0200

    fix: change name to auth context function and wrap mainjsx

--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -4,7 +4,7 @@ const API_URL = "http://localhost:5005";
 
 const AuthContext = React.createContext();
 
-function AuthProviderContext(props) {
+function AuthProviderWrapper(props) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(null);
@@ -18,4 +18,4 @@ function AuthProviderContext(props) {
   );
 }
 
-export { AuthProviderContext, AuthContext };
+export { AuthProviderWrapper, AuthContext };
