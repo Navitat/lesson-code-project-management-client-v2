fe9f3e9eda5ba2186893cae8dc8160ae0f8b939f
navitat <anlon636@gmail.com>
2025-08-04 17:08:30 +0200

    feat: implement signup page and route

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,27 @@ import HomePage from "./pages/HomePage";
 import ProjectListPage from "./pages/ProjectListPage";
 import ProjectDetailsPage from "./pages/ProjectDetailsPage";
 import EditProjectPage from "./pages/EditProjectPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
 
-      <Routes>      
+      <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/projects" element={<ProjectListPage />} />
-        <Route exact path="/projects/:projectId" element={<ProjectDetailsPage />} />
-        <Route exact path="/projects/edit/:projectId" element={<EditProjectPage />} />           
+        <Route
+          exact
+          path="/projects/:projectId"
+          element={<ProjectDetailsPage />}
+        />
+        <Route
+          exact
+          path="/projects/edit/:projectId"
+          element={<EditProjectPage />}
+        />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </div>
   );
